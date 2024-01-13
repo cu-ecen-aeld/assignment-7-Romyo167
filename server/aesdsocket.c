10d9553be4bcc51bb29b0a8ab7a9915a96a1a48a
Ramy Nosir <52046932+Romyo167@users.noreply.github.com>
2024-01-13 10:54:59 +0200

    Update aesdsocket.c

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -237,9 +237,11 @@ int main(int argc, char * argv[]) {
 
             if(prev_sec >= sec - 10)
             {
+                pthread_mutex_lock(&lock);
                 fputs("timestamp:", msgfile);
                 fputs(outstr, msgfile);
                 fputs("\n", msgfile);
+                pthread_mutex_unlock(&lock); 
             } 
             prev_sec = sec;
             fclose(msgfile);
